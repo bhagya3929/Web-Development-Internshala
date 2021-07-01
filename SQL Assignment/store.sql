d95166e209e0b0f29a5455901f726932424e3608
Bhagyashree Bhagyalaxmi <77850791+bhagya3929@users.noreply.github.com>
2021-07-01 14:12:13 +0530

    Create store.sql

---
+++ SQL Assignment/store.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE store;
+
+DROP TABLE IF EXISTS `items`;
+CREATE TABLE IF NOT EXISTS `items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `items` (`id`, `name`, `price`) VALUES
+(1, 'Canon EOS', 36000),
+(2, 'Nikon DSLR', 40000),
+(3, 'Sony DSLR', 45000),
+(4, 'Olympus DSLR', 50000),
+(5, 'Titan Model #301', 13000),
+(6, 'Titan Model #201', 3000),
+(7, 'HMT Milan', 8000),
+(8, 'Faber Luba #111', 18000),
+(9, 'H&W', 800),
+(10, 'Luis Phil', 1000),
+(11, 'John Zok', 1500),
+(12, 'Jhalsani', 1300);
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `user_items`;
+CREATE TABLE IF NOT EXISTS `user_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `STATUS` enum('Added to cart','Confirmed') NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `item_id` (`item_id`)
+);
